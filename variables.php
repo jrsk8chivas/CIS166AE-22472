178cc10dc2633d520bfa5cd83bf92d96ab39e69b
Jose Herrera Medina <jherreram95@outlook.com>
2020-02-09 20:26:01 -0700

    Add files via upload

---
+++ variables.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">  
+<head>
+	<meta charset="utf-8">
+	<title>Variables</title> 
+</head>   
+<body>   
+<pre>  
+<?php //2.3 - variables.php
+
+	$firstName = "Jose";
+	$lastName = "Herrera";
+	$address = "1202 W Thomas Rd, Phoenix, AZ 85013";
+	$phoneNumber = 6022857800;
+	
+	//Print Information
+	print "<p> $firstName <br> $lastName <br> $address <br> $phoneNumber </p>";
+?>  
+</pre> 
+</body> 
+</html>
