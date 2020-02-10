b38dd0f9766e0050c7b585247880fdb6b77d747e
Jose Herrera Medina <jherreram95@outlook.com>
2020-02-09 19:57:50 -0700

    Add files via upload

---
+++ predefined.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">  
+<head>
+	<meta charset="utf-8">
+	<title>Predefined Variables</title> 
+</head>   
+<body>   
+<pre>  
+<?php
+	print_r($_SERVER); 
+?>  
+</pre> 
+</body> 
+</html>
