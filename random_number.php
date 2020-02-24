65ea97d1b3fc382f56773efd86052192eb4e3c68
Jose Herrera Medina <jherreram95@outlook.com>
2020-02-23 21:56:11 -0700

    Add files via upload

---
+++ random_number.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Random Number</title>
+</head>
+<body>
+<?php // Produce a random number	
+$randomNumber = mt_rand(10,100);
+print "Your random number is: $randomNumber";
+?>
+</body>
+</html>
