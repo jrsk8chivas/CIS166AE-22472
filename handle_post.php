dfac9a3cdb16320efa39569c7a7e19b25baf9a2d
Jose Herrera Medina <jherreram95@outlook.com>
2020-03-01 18:44:54 -0700

    Add files via upload

--- handle_post.php
+++ handle_post.php
@@ -5,15 +5,20 @@
 	<title>Forum Posting</title>
 </head>
 <body>
-<?php // Script 5.2 - handle_post.php
+<?php // Script 5.3 - handle_post.php #2
+/* This script receives five values from posting.html : 
+first_name, last_name, email, posting, submit */
 
+//Address error management, if you want
 error_reporting(E_ALL); // display all errors
 
+//Get the values from the $_POST array:
 $first_name = $_POST['first_name'];
 $last_name = $_POST['last_name'];
 $posting = nl2br($_POST['posting'], false);
 
-$name = $first_name .' ' .$last_name;
+//Create a full name variable:
+$name = $first_name . '' . $last_name;
 
 print "<div>Thank you, $name, for your posting: 
 <p>$posting</p></div>";
