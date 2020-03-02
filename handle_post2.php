fe2c8324b8d8a1c6083aed692a20420f5f318571
Jose Herrera Medina <jherreram95@outlook.com>
2020-03-01 19:05:26 -0700

    Add files via upload

--- handle_post2.php
+++ handle_post2.php
@@ -4,27 +4,27 @@
 	<meta charset="utf-8">
 	<title>Forum Posting</title>
 </head>
-<body>
-<?php // Script 5.4 - handle_post1.php
 
-error_reporting(E_ALL); // display all errors
+<body>
+<?php // Script 5.5 - handle_post2.php
+
+error_reporting(0); 
 
 // Get values from the _POST array:
 $first_name = $_POST['first_name'];
 $last_name = $_POST['last_name'];
-$posting = nl2br($_POST['posting'], false);
+$posting = nl2br($_POST['posting']);
 
 // Full name variable:
-$name = $first_name .' ' .$last_name;
-
-// Adjust for HTML tags:
-$html_post = htmlentities($_POST['posting']);
-$strip_post = strip_tags($_POST['posting']);
+$name = $first_name . ' ' . $last_name;
 
 print "<div>Thank you, $name, for your posting: 
-<p>Original: $posting</p>
-<p>Entity: $html_post</p>
-<p>Stripped: $strip_post</p></div>";
+<p>$posting</p></div>";
+
+// Make a link to another page:
+$name = urlencode($name);
+$email = urlencode($_POST['email']);
+print "<p>Click <a href=\"thanks.php?name=$name&email=$email\">here</a> to continue.</p>";
 
 ?>
 </body>
