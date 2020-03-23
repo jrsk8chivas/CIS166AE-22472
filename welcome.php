9a48166258309e1d188f16c708dea58f677feb36
Jose Herrera Medina <jherreram95@outlook.com>
2020-03-22 23:10:09 -0700

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,12 @@
+<?php
+
+define('TITLE', 'Welcome to the J.D. Salinger Fan Club!');
+include('template/header.html');
+
+?>
+
+<h2>Welcome to the J.D. Salinger Fan Club!</h2>
+<p>You've successfully logged in and can now take advantage of everything the site has to offer.</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+<?php include('template/footer1.html');
