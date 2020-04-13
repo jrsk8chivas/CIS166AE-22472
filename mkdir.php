df1f9521ad260f64e90f3425be44fec8d985122a
Jose Herrera Medina <jherreram95@outlook.com>
2020-04-12 23:11:16 -0700

    Add files via upload

---
+++ mkdir.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Create directory</title>
+</head>
+
+<body>
+    <pre>
+            <?php
+            mkdir("/xampp/htdocs/users");
+            print("Created a directory");
+            ?>
+        </pre>
+</body>
+
+</html>
