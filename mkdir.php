655df212872ce5446c103d72313d9767f23624b6
Jose Herrera Medina <jherreram95@outlook.com>
2020-04-12 23:06:58 -0700

    Add files via upload

---
+++ mkdir.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Create directory</title>
+</head>
+
+<body>
+    <pre>
+            <?php
+            mkdir("/opt/lampp/htdocs/users");
+            print("Created a directory");
+            ?>
+        </pre>
+</body>
+
+</html>
